05a1d4905e066aaf1c585030d8ea10e7a0477f3f
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-02-22 23:08:15 -0300

    Codigo Byte Bank

--- src/Conta.java
+++ src/Conta.java
@@ -1,6 +1,12 @@
-public class Conta  {
+public class Conta {
     double saldo;
     int agencia;
     int numero;
     String titular;
+
+    public double depositar(double valor) {
+        return this.saldo = this.saldo + valor;
+
+
+    }
 }
